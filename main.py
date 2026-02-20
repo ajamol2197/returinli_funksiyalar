352c8b9b2d0bbb2cb4866dea6c141080d4ca11b6
ajamol2197 <ajamol2197@gmail.com>
2026-02-20 16:27:41 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,64 @@
+# 1
+def ism(a, b="dost"):
+    return f"salom {a}"
+
+
+print(ism())
+
+# 2
+def mk(a, b):
+    return a ** b
+
+
+print(f"darajasi: {mk(12, 44)}")
+
+
+# 3
+def ism(matn, tom):
+    return matn * tom
+
+
+print(f"takrorlangan son: {ism("ool", 2)}
+
+
+# 4
+def yoshd(b, a):
+    return f"{b, a} yoshda"
+
+
+print(yoshd("asl", 21))
+
+# 5
+def summma(a, b):
+    return a + b
+
+
+print(f"sizni summingiz {summma(12, 5)}")
+
+# 6
+def kop(a, b):
+    return a * b
+
+
+print(f"sizni kopaytmangiz {kop(2, 5)}")
+
+# 7
+def matn(a, b="xabar yoq"):
+    return a, b
+
+
+print(matn(0))
+
+# 8
+def m(matn, cheg=0):
+    return matn > cheg
+
+
+print(m(2))
+
+# 9
+def a(b):
+    return f"xush kelibsiz {a}"
+
+
+print(a("ali"))
